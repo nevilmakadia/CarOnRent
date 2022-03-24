b809cde38dfa7131582a475bf953d30155ca6d96
Nevil Makadia <nevilmakadia@gmail.com>
2022-03-24 18:03:03 +0530

    Update CarOnRentController.php

--- app/Http/Controllers/CarOnRentController.php
+++ app/Http/Controllers/CarOnRentController.php
@@ -71,6 +71,79 @@ class CarOnRentController extends Controller
             }
         }
 
+        // halfDay
+        $if_halfDay = CarOnRent::where([
+            'cityName' => $request->cityName,
+            'carName' => $request->carName,
+            'bookingDate' => $request->bookingDate,
+            'bookingType' => 'halfDay',
+            'halfDay' => '09:00am To 01:00pm',
+            'halfDay' => '02:00pm To 09:00pm'
+        ])->first();
+
+        if (!empty($if_halfDay)) {
+            $request->session()->flash('message', 'This schedule is already booked for halfDays');
+            return redirect('bookCar');
+        }
+
+        $book = new CarOnRent;
+        if ($request->bookingType = 'halfDay') {
+
+            // firstHalf
+            $first_halfDay = CarOnRent::where([
+                'cityName' => $request->cityName,
+                'carName' => $request->carName,
+                'bookingDate' => $request->bookingDate,
+                'bookingType' => 'halfDay',
+                'halfDay' => '09:00am To 01:00pm'
+            ])->first();
+
+            if (!empty($first_halfDay)) {
+                $request->session()->flash('message', 'This schedule is already booked for half day');
+                return redirect('bookCar');
+            } else {
+                $book = new CarOnRent;
+                $book->cityName = $request->cityName;
+                $book->carName = $request->carName;
+                $book->bookingDate = $request->bookingDate;
+                $book->bookingType = $request->bookingType;
+                $book->halfDay = $request->halfDay;
+                $book->hourly = $request->hourly;
+                $book->fromTime = $request->fromTime;
+                $book->toTime = $request->toTime;
+                $book->destination = $request->destination;
+                $book->save();
+                $request->session()->flash('message', 'Your booking has been done for half day');
+                return redirect('viewBooking');
+            }
+            // secondHalf
+            $second_halfDay = CarOnRent::where([
+                'cityName' => $request->cityName,
+                'carName' => $request->carName,
+                'bookingDate' => $request->bookingDate,
+                'bookingType' => 'halfDay',
+                'halfDay' => '02:00pm To 09:00pm'
+            ])->first();
+
+            if (!empty($second_halfDay)) {
+                $request->session()->flash('message', 'This schedule is already booked for half day');
+                return redirect('bookCar');
+            } else {
+                $book = new CarOnRent;
+                $book->cityName = $request->cityName;
+                $book->carName = $request->carName;
+                $book->bookingDate = $request->bookingDate;
+                $book->bookingType = $request->bookingType;
+                $book->halfDay = $request->halfDay;
+                $book->hourly = $request->hourly;
+                $book->fromTime = $request->fromTime;
+                $book->toTime = $request->toTime;
+                $book->destination = $request->destination;
+                $book->save();
+                $request->session()->flash('message', 'Your booking has been done for half day');
+                return redirect('viewBooking');
+            }
+        }
     }
 
     public function show(CarOnRent $carOnRent)
